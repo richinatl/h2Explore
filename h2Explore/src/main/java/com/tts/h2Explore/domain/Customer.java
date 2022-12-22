0e5c409cce08d86969acd215a3e3200e17f78135
Richard Murphy <richinatl@gmail.com>
2022-12-22 11:59:50 -0500

    locally created Spring Boot project

---
+++ h2Explore/src/main/java/com/tts/h2Explore/domain/Customer.java
@@ -0,0 +1,42 @@
+package com.tts.h2Explore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+	
+	    @Id
+	    @GeneratedValue(strategy=GenerationType.AUTO)
+	    private Long id;
+	    private String firstName;
+	    private String lastName;
+		public Customer(String string, String string2) {
+			// TODO Auto-generated constructor stub
+			this.firstName = string;
+			this.lastName = string2;
+		}
+		
+		public Customer() {
+			firstName = getFirstName();
+			lastName = getLastName();
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		@Override
+		public String toString() {
+			return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+		} 
+}
